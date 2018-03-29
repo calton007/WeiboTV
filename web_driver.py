fecc94e5e842ba00c6a5065ce29aac1479ebf948
calton007 <18347082+calton007@users.noreply.github.com>
2018-03-29 10:46:17 +0800

    Add description
    'web_driver.py' is used to get cookies and save as a file named 'cookies.json'
    It uses selenium and Chrome's web driver as the import.
    Before getting cookies, you need to set a Weibo ACCOUNT and PASSWORD data in users.py

--- web_driver.py
+++ web_driver.py
@@ -7,28 +7,28 @@ class GetCookies:
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
-        self.driver.get('http://weibo.com/login.php')
+        self.driver.get('http://weibo.com/login.php') # load the login page
         time.sleep(10)
 
     def run(self):
         try:
-            self.driver.find_element_by_xpath('//div[@class="info_list username"]/div/input').send_keys(ACCOUNT)
+            self.driver.find_element_by_xpath('//div[@class="info_list username"]/div/input').send_keys(ACCOUNT) # ACCOUNT should be defined in users.py
             print('input username')
         except:
             print('username error!')
         time.sleep(3)
         try:
-            self.driver.find_element_by_xpath('//div[@class="info_list password"]/div/input').send_keys(PASSWORD)
+            self.driver.find_element_by_xpath('//div[@class="info_list password"]/div/input').send_keys(PASSWORD) # ACCOUNT should be defined in users.py
             print('input password')
         except:
             print('password error!')
         time.sleep(3)
         try:
-            self.driver.find_element_by_xpath('//div[@class="info_list login_btn"]/a').click()
+            self.driver.find_element_by_xpath('//div[@class="info_list login_btn"]/a').click() # click the botton to login
             print('click to login')
         except:
             print('click error!')
-        time.sleep(10)
+        time.sleep(10) # wait to login
         cookies={"cookies":[]}
         for item in self.driver.get_cookies():
             cookies["cookies"].append({"name":item["name"], "value":item["value"]})
